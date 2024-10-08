56b32f103dd2662fccc55aa11e86cd09776dcbe3
shajumohanrajg <71750044+shajumohanrajg@users.noreply.github.com>
2024-10-09 00:15:12 +0530

    Update views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers import UserWishlistSerializer
-from rest_framework import permissions
-from .models import UserWishlist
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from authentication.models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from rest_framework import generics, permissions
+from .serializers import UserWishlistSerializer
+from .models import UserWishlist
 
 
 class UserWishlistDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -19,30 +15,23 @@ class UserWishlistDetail(generics.RetrieveUpdateDestroyAPIView):
         # Filter the wishlist items by the authenticated user
         return UserWishlist.objects.filter(wishlist_user=self.request.user)
 
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()  # Fetch the instance using the ID from the URL
+        self.perform_destroy(instance)  # Delete the instance
+        
+        # Customize the response after deletion
+        return Response({"detail": "Wishlist item deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
 
-# class UserWishlistList(generics.ListCreateAPIView):
-#     permission_classes = [permissions.IsAuthenticated]
-#     authentication_classes = [JWTAuthentication]
-#     serializer_class = UserWishlistSerializer
 
-#     def get_queryset(self):
-#         # Return all wishlist items for the authenticated user
-#         return UserWishlist.objects.filter(wishlist_user=self.request.user)
-
-#     def perform_create(self, serializer):
-#         # Automatically associate the wishlist with the logged-in user
-#         serializer.save(wishlist_user=self.request.user)
 
 class UserWishlistCreateAPIView(generics.ListCreateAPIView):
     serializer_class = UserWishlistSerializer
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
-
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['request'] = self.request
-        return context
-
     def get_queryset(self):
         return UserWishlist.objects.filter(wishlist_user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically associate the wishlist with the logged-in user
+        serializer.save(wishlist_user=self.request.user)
