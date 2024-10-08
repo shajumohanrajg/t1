4524284b01de13030e131e008d2b090298c6e770
shajumohanrajg <71750044+shajumohanrajg@users.noreply.github.com>
2024-10-09 00:14:42 +0530

    Update urls.py

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,11 +1,18 @@
+# from django.urls import path
+# from . import views
 from django.urls import path
-from . import views
+from .views import UserWishlistCreateAPIView, UserWishlistDetail
 
+
+# urlpatterns = [
+#     # path('wishlist/', views.WishlistAPIView.as_view(), name='wishlist'),
+#     # path('wishlist/<int:id>/', views.WishlistDetailAPIView.as_view(), name='wishlist-detail')
+#     # path('wishlists/<int:pk>/', views.UserWishlistView.as_view(), name='user_wishlist'),
+#     path('wishlists/', views.UserWishlistCreateAPIView.as_view(), name='user_wishlists'),
+#     path('wishlists/<int:pk>/', views.UserWishlistDetail.as_view(), name='user_wishlist'),
+# ]
 
 urlpatterns = [
-    # path('wishlist/', views.WishlistAPIView.as_view(), name='wishlist'),
-    # path('wishlist/<int:id>/', views.WishlistDetailAPIView.as_view(), name='wishlist-detail')
-    # path('wishlists/<int:pk>/', views.UserWishlistView.as_view(), name='user_wishlist'),
-    path('wishlists/', views.UserWishlistCreateAPIView.as_view(), name='user_wishlists'),
-    path('wishlists/<int:pk>/', views.UserWishlistDetail.as_view(), name='user_wishlist'),
+    path('wishlists/', UserWishlistCreateAPIView.as_view(), name='wishlist-list-create'),
+    path('wishlists/<int:pk>/', UserWishlistDetail.as_view(), name='wishlist-detail'),
 ]
